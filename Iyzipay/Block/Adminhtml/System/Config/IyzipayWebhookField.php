c5c0ac4db37ab81d85e6b6f4b33a2cfaa132c871
tarikkamat <tarikkamat2@gmail.com>
2024-08-16 14:24:23 +0300

    log updated

--- Iyzipay/Block/Adminhtml/System/Config/IyzipayWebhookField.php
+++ Iyzipay/Block/Adminhtml/System/Config/IyzipayWebhookField.php
@@ -29,7 +29,7 @@ use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Store\Model\ScopeInterface;
 use Iyzico\Iyzipay\Logger\IyziErrorLogger;
-
+use Magento\Backend\Block\Template\Context;
 /**
  * Class GetWebhookUrlField
  *
@@ -42,6 +42,17 @@ class IyzipayWebhookField extends Field
 {
     protected StoreManagerInterface $storeManager;
     protected IyziErrorLogger $logger;
+
+    public function __construct(
+        Context $context,
+        StoreManagerInterface $storeManager,
+        IyziErrorLogger $logger,
+        array $data = []
+    ) {
+        $this->storeManager = $storeManager;
+        $this->logger = $logger;
+        parent::__construct($context, $data);
+    }
 
     private function getStoreId(): int
     {
