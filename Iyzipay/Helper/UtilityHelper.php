3f2f639396c5d85b7c2c7d74a5423becf2bd6c12
Tarık KAMAT <tarikkamat2@gmail.com>
2024-12-20 15:35:51 +0300

    Refactor UtilityHelper to improve code readability, remove unused methods, and add order validation and retrieval functionality

--- Iyzipay/Helper/UtilityHelper.php
+++ Iyzipay/Helper/UtilityHelper.php
@@ -33,7 +33,7 @@ class UtilityHelper
     public function parsePrice(float $price): string
     {
         if (strpos($price, ".") === false) {
-            return $price . ".0";
+            return $price.".0";
         }
 
         $subStrIndex = 0;
@@ -43,7 +43,7 @@ class UtilityHelper
                 $subStrIndex = $i + 1;
             } else {
                 if (strcmp($priceReversed[$i], ".") == 0) {
-                    $priceReversed = "0" . $priceReversed;
+                    $priceReversed = "0".$priceReversed;
                     break;
                 } else {
                     break;
@@ -52,23 +52,6 @@ class UtilityHelper
         }
 
         return strrev(substr($priceReversed, $subStrIndex));
-    }
-
-    /**
-     * Calculate Subtotal Price
-     *
-     * @param  $order
-     * @return string
-     */
-    public function calculateSubTotalPrice($order): string
-    {
-        $price = 0;
-        foreach ($order->getAllVisibleItems() as $item) {
-            $price += round($item->getPrice(), 2);
-        }
-
-        $price += $order->getShippingAddress()->getShippingAmount() ?? 0;
-        return $this->parsePrice($price);
     }
 
     /**
@@ -109,10 +92,13 @@ class UtilityHelper
     /**
      * Apply Cookie Settings
      *
-     * @param  string  $name  Name of the cookie
-     * @param  string  $value  Value of the cookie
-     * @param  int  $expire  Expiration time
-     * @param  string  $domain  Domain for the cookie
+     * This function is responsible for applying the cookie settings.
+     *
+     * @param  string  $name
+     * @param  string  $value
+     * @param  int  $expire
+     * @param  string  $domain
+     *
      * @return void
      */
     private function applyCookieSettings(string $name, string $value, int $expire, string $domain): void
@@ -132,19 +118,9 @@ class UtilityHelper
     }
 
     /**
-     * Concatenate Strings
-     *
-     * @param  string  ...$address
-     * @return string
-     */
-    public function concatenateStrings(string ...$address): string
-    {
-        $address = array_map('trim', $address);
-        return implode(' ', $address);
-    }
-
-    /**
      * Validate String
+     *
+     * This function is responsible for validating the string.
      *
      * @param  mixed  $string
      * @return string
@@ -165,16 +141,20 @@ class UtilityHelper
     /**
      * Generate Conversation Id
      *
+     * This function is responsible for generating the conversation ID.
+     *
      * @param  int  $quoteId
      * @return string
      */
     public function generateConversationId(int $quoteId): string
     {
-        return 'QI' . $quoteId . 'T' . time();
+        return 'QI'.$quoteId.'T'.time();
     }
 
     /**
      * Get Customer Id
+     *
+     * This function is responsible for getting the customer ID.
      *
      * @param  CustomerSession  $customerSession
      * @return int|null
@@ -210,6 +190,8 @@ class UtilityHelper
     /**
      * Get Locale Name
      *
+     * This function is responsible for getting the locale name.
+     *
      * @param $locale
      * @return string
      */
@@ -228,10 +210,8 @@ class UtilityHelper
      * @param  CustomerSession  $customerSession
      * @return void
      */
-    public function storeSessionData(
-        Quote $checkoutSession,
-        CustomerSession $customerSession
-    ): void {
+    public function storeSessionData(Quote $checkoutSession, CustomerSession $customerSession): void
+    {
         $customerEmail = $checkoutSession->getBillingAddress()->getEmail();
         $quoteId = $checkoutSession->getId();
         $checkoutSession->setGuestQuoteId($quoteId);
@@ -278,6 +258,8 @@ class UtilityHelper
     /**
      * Calculate HMAC SHA256 Signature
      *
+     * This function is responsible for calculating the HMAC SHA256 signature.
+     *
      * @param  array  $params
      * @param  string  $secretKey
      * @return string
@@ -290,4 +272,76 @@ class UtilityHelper
         return bin2hex($mac);
     }
 
+    /**
+     * Validate Conversation ID
+     *
+     * This function is responsible for validating the conversation ID.
+     *
+     * @param  string  $conversationId
+     * @param  string  $responseConversationId
+     * @return bool
+     */
+    public function validateConversationId(string $conversationId, string $responseConversationId): bool
+    {
+        if ($conversationId !== $responseConversationId) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Find Order By State And Status
+     *
+     * This function is responsible for finding the order by state and status.
+     *
+     * @param  string  $responsePaymentStatus
+     * @param  string  $responseStatus
+     * @return array
+     */
+    public function findOrderByPaymentAndStatus(string $responsePaymentStatus, string $responseStatus): array
+    {
+        $ordersByPaymentAndStatus = [
+            'state' => '',
+            'status' => '',
+            'comment' => '',
+            'orderJobStatus' => ''
+        ];
+
+        if ($responsePaymentStatus == 'PENDING_CREDIT' && $responseStatus == 'success') {
+            $ordersByPaymentAndStatus['state'] = 'pending_payment';
+            $ordersByPaymentAndStatus['status'] = 'pending_payment';
+            $ordersByPaymentAndStatus['comment'] = __('PENDING_CREDIT');
+            $ordersByPaymentAndStatus['orderJobStatus'] = 'pending_payment';
+        }
+
+        if ($responsePaymentStatus == 'INIT_BANK_TRANSFER' && $responseStatus == 'success') {
+            $ordersByPaymentAndStatus['state'] = 'pending_payment';
+            $ordersByPaymentAndStatus['status'] = 'pending_payment';
+            $ordersByPaymentAndStatus['comment'] = __('INIT_BANK_TRANSFER');
+            $ordersByPaymentAndStatus['orderJobStatus'] = 'pending_payment';
+        }
+
+        if ($responsePaymentStatus == 'INIT_THREEDS' && $responseStatus == 'success') {
+            $ordersByPaymentAndStatus['state'] = 'pending_payment';
+            $ordersByPaymentAndStatus['status'] = 'pending_payment';
+            $ordersByPaymentAndStatus['comment'] = __('INIT_THREEDS_CRON');
+            $ordersByPaymentAndStatus['orderJobStatus'] = 'pending_payment';
+        }
+
+        if ($responsePaymentStatus == 'SUCCESS' && $responseStatus == 'success') {
+            $ordersByPaymentAndStatus['state'] = 'processing';
+            $ordersByPaymentAndStatus['status'] = 'processing';
+            $ordersByPaymentAndStatus['comment'] = __('SUCCESS');
+            $ordersByPaymentAndStatus['orderJobStatus'] = 'processing';
+        }
+
+        if ($responsePaymentStatus == 'FAILURE') {
+            $ordersByPaymentAndStatus['state'] = 'canceled';
+            $ordersByPaymentAndStatus['status'] = 'canceled';
+            $ordersByPaymentAndStatus['comment'] = __('FAILURE');
+            $ordersByPaymentAndStatus['orderJobStatus'] = 'canceled';
+        }
+
+        return $ordersByPaymentAndStatus;
+    }
 }
